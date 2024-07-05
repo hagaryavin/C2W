fa59b1ced1f943fdec4bdf45671d8ba66f1ccf32
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2024-07-05 17:58:48 +0300

    Update meta.js

--- meta.js
+++ meta.js
@@ -388,7 +388,9 @@ function fixFirstName(fullName) {
     splittedName[0] === "דוקטור" ||
     splittedName[0] === "פרופסור" ||
     splittedName[0] === "פרופ'" ||
-    splittedName[0] === "Dr."
+    splittedName[0] === "Dr." ||
+     splittedName[0] === "הרב" ||
+     splittedName[0] === "ד״ר" 
   ) {
     return splittedName[1];
   }
