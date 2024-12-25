216b50fdcc2d60b826a74cdcbd3e15f43abffd86
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2024-12-25 10:31:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ function getData() {
             newPerson.chain = ele.chaintwo;
           }
           if (ele.chainthree !== "") newPerson.chain = ele.chainthree;
+          if (ele.chainfour !== "") newPerson.chain = ele.chainfour;
         }
         if (ele.fixedchain !== "") newPerson.chain = ele.fixedchain; 
         if (ele.recordingdate !== "")
